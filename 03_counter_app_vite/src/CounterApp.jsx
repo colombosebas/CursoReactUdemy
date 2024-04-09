586962ac06ae52eb36e11c5fdbd27c3913f92023
colombosebas <colombosebas@gmail.com>
2024-04-09 19:38:24 -0300

    Class 47

--- 03_counter_app_vite/src/CounterApp.jsx
+++ 03_counter_app_vite/src/CounterApp.jsx
@@ -8,12 +8,16 @@ export const CounterApp = ({value}) => {
     const handleAdd = (event) => {
         setCounter(counter + 1)
     }
+    const handleSubstract = (event) => setCounter(counter - 1) 
+    const handleReset = (event) => setCounter(value)
     return(
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
             <h2>{counter}</h2>
-            <button onClick={handleAdd}>1</button>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubstract}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 }
